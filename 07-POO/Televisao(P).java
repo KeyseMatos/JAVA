d273cd0c4ed3eadb4b0ddb5ba9275fc7e525cea2
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-04 08:43:48 -0300

    Add Principal class to demonstrate Televisao usage

---
+++ 07-POO/Televisao(P).java
@@ -0,0 +1,20 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Televisao samsung = new Televisao();
+		samsung.status();
+		samsung.ligar();
+		samsung.aumentarCanal();
+		samsung.aumentarVolume();
+		samsung.status();
+		samsung.ligar();
+		samsung.aumentarVolume();
+		samsung.status();
+		
+		
+
+	}
+
+}
