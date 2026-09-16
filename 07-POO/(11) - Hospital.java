6513947e3e5421c6c99aa6e6ac950af1c9add641
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-16 08:38:13 -0300

    Create Hospital class with ArrayList of Medico

---
+++ 07-POO/(11) - Hospital.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Hospital {
+	
+	ArrayList<Medico> medicos = new ArrayList<Medico>();
+	
+//	public static void main(String[] args) {
+//		
+//		Medico clinico = new Medico("Guilherme", "1234BA", 54, 12500);
+//		MedicoAuxiliar mAux = new MedicoAuxiliar("Eduarda", "1233", 60, 15000);
+//		MedicoCirurgiao mC = new MedicoCirurgiao("John", "2424BA", 50, 25000);
+//		
+//		System.out.println(clinico.medicoAposentado());
+//		System.out.println(mAux.medicoAposentado());
+//		System.out.println(mC.medicoAposentado());
+//		System.out.println(clinico.valorAposentadoria());
+//		System.out.println(mAux.valorAposentadoria());
+//		System.out.println(mC.valorAposentadoria());
+//		
+//		System.out.println(clinico);
+//		System.out.println(mAux);
+//		System.out.println(mC);
+//		
+//	}
+	
+}
