9c465427a8688cbc993c4f0fb7ed5a6c3cd63ef3
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-16 13:44:21 -0300

    Update (11) - Hospital(Medico).java

--- 07-POO/(11) - Hospital(Medico).java
+++ 07-POO/(11) - Hospital(Medico).java
@@ -1,61 +1,59 @@
-public class Medico {
+//Classe
+public abstract class Medico{
 
-	private String nome;
-	private String CRM;
-	private int idade;
-	private double salario;
+    //Atributos da classe
+    private String CRM;
+    private String nome;
+    private int idade;
+    private double salario;
 
-	@Override
-	public String toString() {
-		return "Medico [nome=" + nome + ", CRM=" + CRM + ", idade=" + idade + ", salario=" + salario + "]";
-	}
+    @Override
+    public String toString(){
+        return "Medico [nome=" + nome + ", CRM= "+ CRM + ", idade= "+ idade + ", salario= "+ salario + "]";
+    }
 
-	public Medico(String nome, String cRM, int idade, double salario) {
-		super();
-		this.nome = nome;
-		CRM = cRM;
-		this.idade = idade;
-		this.salario = salario;
-	}
+    //Construtor
+    public Medico(String CRM, String nome, int idade, double salario){
+        super();
+        this.CRM = CRM;
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+    }
 
-	public boolean medicoAposentado() {
-		return idade >= 55;
-	}
-
-	public double valorAposentadoria() {
-		return salario * 0.8;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCRM() {
-		return CRM;
-	}
-
-	public void setCRM(String cRM) {
-		CRM = cRM;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public double getSalario() {
-		return salario;
-	}
-
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-
+    //Declaração dos métodos abstratos
+    public abstract boolean medicoAposentado(){
+        return idade >= 55;
+    }
+        
+    public abstract double valorAposentadoria(){
+        return salario * 0.8;
+    }
+        
+    //Get
+    public String  getCRM(){
+        return CRM;
+    }
+    //Set
+    public void setCRM(String CRM){
+        this.CRM = CRM;
+    }
+    public String(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public double getSalario(){
+        return salario;
+    }
+    public void setSalario(double salario){
+        salario(salario);
+    }
 }
