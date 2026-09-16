b921f6b07401b60e7233c64988e5eed00593b69b
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-16 13:45:14 -0300

    Update (11) - Hospital(MedicoCirurgiao).java

--- 07-POO/(11) - Hospital(MedicoCirurgiao).java
+++ 07-POO/(11) - Hospital(MedicoCirurgiao).java
@@ -1,17 +1,17 @@
-public class MedicoCirurgiao extends Medico {
+public class MedicoCirurgiao extends Medico{
 
-	public MedicoCirurgiao(String nome, String cRM, int idade, double salario) {
-		super(nome, cRM, idade, salario);
-	}
-
-	@Override
-	public boolean medicoAposentado() {
-		return getIdade() >= 50;
-	}
-
-	@Override
-	public double valorAposentadoria() {
-		return super.valorAposentadoria() + 800;
-	}
-
+    //Vai pegar as informações da classe mãe médico para utilizar aqui por isso o super
+    public MedicoCirurgiao(String CRM, String nome, int idade, double salario){
+        super(CRM, nome, idade, salario);
+    }
+    //Escreva em cima de medicoAposentado que está na classe médico o que irei lhe passar
+    @Override
+    public boolean medicoAposentado(){
+        return getIdade() >= 50;
+    }
+    @Override
+    public double valorAposentadoria(){
+        return super.valorAposentadoria() + 800;
+        //o super ele vai chamar a aposentadoria na classe medico que já tem o calculo e vou aumentar os 800
+    }
 }
