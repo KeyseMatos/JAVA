574b316958ae9e1b46905494edb763c127c96e2c
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-18 15:00:13 -0300

    Create (12) - Professor(Universidade).java

---
+++ 07-POO/(12) - Professor(Universidade).java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Universidade{
+
+    ArrayList<Professor>professores = new ArrayList<Professor>();
+
+        public void adicionarProfessores(Professor professor){
+            professores.add(professor);
+        }
+
+        
+}
