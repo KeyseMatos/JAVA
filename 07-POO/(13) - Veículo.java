73cabe560bb000ea3651503457f9a9a234ca8786
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-09-21 21:29:10 -0300

    Create (13) - Veículo.java

---
+++ 07-POO/(13) - Veículo.java
@@ -0,0 +1,59 @@
+
+public class Veiculo {
+	
+	private String placa;
+	private int horaEstacionamento;
+	public static int totalVeiculosAtendidos;
+	
+	//CONSTRUTOR
+	public Veiculo(String placa, int horaEstacionamento) {
+		super();
+		this.placa = placa;
+		this.horaEstacionamento = horaEstacionamento;
+		//INCREMENTAR
+		totalVeiculosAtendidos++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Veiculo [placa=" + placa + ", horaEstacionamento=" + horaEstacionamento + ", calcularValorTotal()="
+				+ calcularValorTotal() + ", getPlaca()=" + getPlaca() + ", getHoraEstacionamento()="
+				+ getHoraEstacionamento() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+	public abstract double calcularValorTotal();
+	public abstract boolean validarPlaca() {
+		if(placa == placa.length() == 7) {
+			return true;
+		}
+	}
+
+	//GET E SETTER
+	public String getPlaca() {
+		return placa;
+	}
+
+
+	public void setPlaca(String placa) {
+		if(placa !=  null && placa.length() == 7 ) {
+			this.placa = placa;
+		};
+	}
+
+
+	public int getHoraEstacionamento() {
+		return horaEstacionamento;
+	}
+
+
+	public void setHoraEstacionamento(int horaEstacionamento) {
+		this.horaEstacionamento = horaEstacionamento;
+	}
+	
+	
+	
+	
+
+}
+
